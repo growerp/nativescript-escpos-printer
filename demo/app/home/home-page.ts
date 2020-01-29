f27b6d365971f61b56e75fc77bfcd416eda8a1dc
Hans Bakker <h.bakker@antwebsystems.com>
2020-01-29 11:39:52 +0700

    imported ichi printer in standard NativeScript plugin seed created initial demo which prints on local host successful

--- demo/app/home/home-page.ts
+++ demo/app/home/home-page.ts
@@ -1,10 +1,35 @@
-import { EscposPrinter } from 'nativescript-escpos-printer';
-console.log(new EscposPrinter().message);
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+import { PrintClient } from 'nativescript-escpos-printer';
+
+// new Printer Client, param:  0: TCP, 1: USB, 2: Bluetooth
+var printClient = new PrintClient(0);
+printClient.onData = (data: Array<number>) => {
+    console.log("Data from Printer: ", data);
+};
+printClient.onError = (id: number, message: string) => {
+    console.log("Print client error for action #", id, ": ", message);
+};
+printClient.onConnected = (id: number) => {
+    console.log("Print client connected action #: ", id);
+    var message = "Print test String!";
+    var bytes = [];
+    for (var i = 0; i < message.length; i++) {
+        var c = message.charCodeAt(i);
+        bytes.push(c & 0xFF);
+    }
+    printClient.send(bytes);
+};
+printClient.onSent = (id: number) => {
+    console.log("Print client sent action #: ", id);
+    // When we are finished
+    printClient.close();
+};
+printClient.onClosed = (id: number) => {
+    console.log("Print client closed action #: ", id);
+};
+
+console.log("printing now......")
+printClient.connect("10.0.2.2", 9100);
+
 
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 
